fe92b3ff66b984aaedda96ef765404f6d4894c86
Andrii Padalko <a.padalko@gmail.com>
2024-04-07 00:15:33 +0300

    add classes City and Artist to models.py

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    city_name = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("city_name",)
+        verbose_name_plural = "cities"
+
+    def __str__(self):
+        return f"{self.city_name}, {self.country}"
+
+
+class Artist(AbstractUser):
+    city = models.ForeignKey(
+        City,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="cities"
+    )
+
+    class Meta:
+        ordering = ("username",)
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"

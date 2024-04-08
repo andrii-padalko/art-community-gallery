f502c05c0ce9719b5b75af2270e7f15f4c64585c
Andrii Padalko <a.padalko@gmail.com>
2024-04-08 17:12:17 +0300

    register all models in admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from gallery.models import City, Genre, Material, Painting, Artist
+
+
+@admin.register(Artist)
+class ArtistAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("city",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("city",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "city",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Painting)
+class PaintingAdmin(admin.ModelAdmin):
+    search_fields = ("title",)
+    list_filter = ("artist",)
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    search_fields = ("city_name",)
+    list_filter = ("country",)
+
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    search_fields = ("material_name",)
+
+
+@admin.register(Genre)
+class CityAdmin(admin.ModelAdmin):
+    search_fields = ("genre_name",)

9a51230b88a97d175923863f01ecc053714d26cd
Andrii Padalko <a.padalko@gmail.com>
2024-04-28 18:51:57 +0300

    modify .gitignore and settings

--- art_community_gallery/settings.py
+++ art_community_gallery/settings.py
@@ -127,9 +127,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [BASE_DIR / "static", ]
 
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
